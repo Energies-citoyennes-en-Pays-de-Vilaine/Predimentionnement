91a8d8ad7b64b02f63c24d8ead9934deb91603d3
youen froger <youen.froger2@gmail.com>
2022-05-20 13:05:58 +0200

    add default battery configuration

--- configuration.py
+++ configuration.py
@@ -9,6 +9,8 @@ PRODUCTION_SCALING_FACTOR = 1e6  / (365*24) / (CA_PONTCHATEAU_POPULATION + CA_RE
 ADD_SOLAR = True
 BIOENERGY_TOTAL_PROD = (18160) # production in Redon and Pontchateau over the course of the 2020 year
 ADD_BIOENERGY = True
+HAS_BATTERY   = False
+BATTERY_CAPACITY = 0.0
 
 @dataclass(init=True)
 class ConfigObject():
@@ -22,15 +24,20 @@ class ConfigObject():
 	BIOENERGY_TOTAL_PROD      : int
 	ADD_BIOENERGY             : bool
 	PROD_PER_WINDTURBINE      : float
+	HAS_BATTERY               : bool
+	BATTERY_CAPACITY          : float
 config = ConfigObject(
-	NB_EOLIENNE               =  NB_EOLIENNE               ,
-	CA_REDON_POPULATION       =  CA_REDON_POPULATION       ,
-	CA_PONTCHATEAU_POPULATION =  CA_PONTCHATEAU_POPULATION ,
-	NB_PARTICULIERS           =  NB_PARTICULIERS           ,
-	SOLAR_TOTAL_PROD          =  SOLAR_TOTAL_PROD          ,
-	PRODUCTION_SCALING_FACTOR =  PRODUCTION_SCALING_FACTOR ,
-	ADD_SOLAR                 =  ADD_SOLAR                 ,
-	BIOENERGY_TOTAL_PROD      =  BIOENERGY_TOTAL_PROD      ,
-	ADD_BIOENERGY             =  ADD_BIOENERGY            ,
-	PROD_PER_WINDTURBINE      = PROD_PER_WINDTURBINE,  
+	NB_EOLIENNE               = NB_EOLIENNE               ,
+	CA_REDON_POPULATION       = CA_REDON_POPULATION       ,
+	CA_PONTCHATEAU_POPULATION = CA_PONTCHATEAU_POPULATION ,
+	NB_PARTICULIERS           = NB_PARTICULIERS           ,
+	SOLAR_TOTAL_PROD          = SOLAR_TOTAL_PROD          ,
+	PRODUCTION_SCALING_FACTOR = PRODUCTION_SCALING_FACTOR ,
+	ADD_SOLAR                 = ADD_SOLAR                 ,
+	BIOENERGY_TOTAL_PROD      = BIOENERGY_TOTAL_PROD      ,
+	ADD_BIOENERGY             = ADD_BIOENERGY             ,
+	PROD_PER_WINDTURBINE      = PROD_PER_WINDTURBINE      ,
+	HAS_BATTERY               = HAS_BATTERY               ,
+	BATTERY_CAPACITY          = BATTERY_CAPACITY          ,
+  
 )
