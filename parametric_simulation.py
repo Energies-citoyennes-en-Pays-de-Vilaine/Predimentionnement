8f679a5c98af1ac04ee834ebbdb65971547127ad
youen froger <youen.froger2@gmail.com>
2022-05-30 17:17:47 +0200

    add of multiprocess simulation (will require cleanup)

--- parametric_simulation.py
+++ parametric_simulation.py
@@ -2,24 +2,26 @@ from sim import *
 from dataLoader import *
 from configuration import config
 from sys import argv, getsizeof
+import time
+from multiprocessing import Process, Manager
 PARAMS = {
-	"wind_min"               : 1, #average wind prod in MW
-	"wind_max"               : 10,
-	"wind_nb_points"         : 10,
+	"wind_min"               : 5, #average wind prod in MW
+	"wind_max"               : 30,
+	"wind_nb_points"         : 50,
 	"sun_min"                : 1, #average solar prod in MW
-	"sun_max"                : 10,
-	"sun_nb_points"          : 10,
+	"sun_max"                : 40,
+	"sun_nb_points"          : 30,
 	"bio_min"                : 1, #average bioenergy prod in MW (Methanol)
-	"bio_max"                : 10,
-	"bio_nb_points"          : 10,
+	"bio_max"                : 20,
+	"bio_nb_points"          : 40,
 	"flex_min"               : 0.01, #flexibility in %
 	"flex_max"               : 0.1,
-	"flex_nb_points"         : 10,
-	"battery_min"            : 1, #battery capacity in MWh
+	"flex_nb_points"         : 40,
+	"battery_min"            : 0, #battery capacity in MWh
 	"battery_max"            : 10,
-	"battery_nb_points"      : 10,
-	"scaling_factor_for_pop" : 1e6 / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION)
-	
+	"battery_nb_points"      : 40,
+	"scaling_factor_for_pop" : 1e6 / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION),
+	"thread_count"           : 1000,
 }
 if (len(argv) < 2):
 	print("you need to specify the result file location")
@@ -89,30 +91,86 @@ def get_total_size(obj):
 			size += getsizeof(var)
 	return size
 
-	
+manager = Manager()
+thread_sims_to_do    = [[] for i in range(PARAMS["thread_count"])]
+thread_sim_params    = [sim_params.get_clone() for i in range(PARAMS["thread_count"])]
+thread_results       = manager.list()
+running_thread_count = 0
 print("size", get_total_size(sim_params))
 current_sim_count = 0
-with open(out_file_path, "w" ) as out_file:
-	print("wind turbines(W/house)", "solar pannels(W/house)", "bioenergy(W/house)", "battery(Wh/house)", "flexibility (raw ratio)", sep=";", file=out_file, end="")
-	print("","import_avg(W/house)", "export_avg(W/house)", "autosufficiency(%)", sep=";", file=out_file)
-	for wind in range(PARAMS["wind_nb_points"]):
+
+for wind in range(PARAMS["wind_nb_points"]):
 		for sun in range(PARAMS["sun_nb_points"]):
 			for bio in range(PARAMS["bio_nb_points"]):
 				for battery in range(PARAMS["battery_nb_points"]):
 					for flex in range(PARAMS["flex_nb_points"]):
-						print("simulation", current_sim_count, "out of", total_sim_count)
-						wind_to_sim    = PARAMS["wind_min"] + (PARAMS["wind_max"] - PARAMS["wind_min"]) * wind
-						bio_to_sim     = PARAMS["bio_min"] + (PARAMS["bio_max"] - PARAMS["bio_min"]) * bio
-						sun_to_sim     = PARAMS["sun_min"] + (PARAMS["sun_max"] - PARAMS["sun_min"]) * sun
-						battery_to_sim = PARAMS["battery_min"] + (PARAMS["battery_max"] - PARAMS["battery_min"]) * battery
-						flex_to_sim    = PARAMS["flex_min"] + (PARAMS["flex_max"] - PARAMS["flex_min"]) * flex
-						sim_params.wind_power        = wind_to_sim    * PARAMS["scaling_factor_for_pop"]
-						sim_params.bioenergy_power   = bio_to_sim     * PARAMS["scaling_factor_for_pop"]
-						sim_params.solar_power       = sun_to_sim     * PARAMS["scaling_factor_for_pop"]
-						sim_params.battery_capacity  = battery_to_sim * PARAMS["scaling_factor_for_pop"]
-						sim_params.flexibility_ratio = flex_to_sim    * PARAMS["scaling_factor_for_pop"]
-						sim_params.check_and_convert_params()
-						results = simulate_senario(sim_params)
-						print(wind, sun, bio, battery, flex, sep=";", file=out_file, end="")
-						print("",results.imported_power.get_average(), results.exported_power.get_average(), (results.total_production / results.total_consumption).get_average(), sep=";", file=out_file, flush=True)
+						#print("simulation", current_sim_count, "out of", total_sim_count)
+						wind_to_sim    = (PARAMS["wind_min"]    + (PARAMS["wind_max"]    - PARAMS["wind_min"])    * wind    / PARAMS["wind_nb_points"])    * PARAMS["scaling_factor_for_pop"]
+						bio_to_sim     = (PARAMS["bio_min"]     + (PARAMS["bio_max"]     - PARAMS["bio_min"])     * bio     / PARAMS["bio_nb_points"])     * PARAMS["scaling_factor_for_pop"]
+						sun_to_sim     = (PARAMS["sun_min"]     + (PARAMS["sun_max"]     - PARAMS["sun_min"])     * sun     / PARAMS["sun_nb_points"])     * PARAMS["scaling_factor_for_pop"]
+						battery_to_sim = (PARAMS["battery_min"] + (PARAMS["battery_max"] - PARAMS["battery_min"]) * battery / PARAMS["battery_nb_points"]) * PARAMS["scaling_factor_for_pop"]
+						flex_to_sim    = (PARAMS["flex_min"]    + (PARAMS["flex_max"]    - PARAMS["flex_min"])    * flex    / PARAMS["flex_nb_points"])    * PARAMS["scaling_factor_for_pop"]
+						thread_sims_to_do[current_sim_count%PARAMS["thread_count"]].append({
+							"wind_to_sim"    : wind_to_sim,
+							"bio_to_sim"     : bio_to_sim, 
+							"sun_to_sim"     : sun_to_sim,
+							"battery_to_sim" : battery_to_sim,
+							"flex_to_sim"    : flex_to_sim,
+						})
 						current_sim_count += 1
+
+def sim_process_function(i, params_to_sim, sim_param, sim_results):
+	print("process", i, "has started")
+	results = []
+	sim_param : SimParams = thread_sim_params[i]
+	for j in range(len(params_to_sim)):
+		if (i == 0):
+			print("thread 0 is simming", j+1, "out of", len(params_to_sim))
+		param = params_to_sim[j]
+		sim_param.solar_power = param["sun_to_sim"]
+		sim_param.wind_power = param["wind_to_sim"]
+		sim_param.bioenergy_power = param["bio_to_sim"]
+		sim_param.battery_capacity = param["battery_to_sim"]
+		sim_param.flexibility_ratio = param["flex_to_sim"]
+		sim_param.check_and_convert_params()
+		result = simulate_senario(sim_param)
+		result = {**param,
+			"imported_power" : result.imported_power.get_average(),
+			"exported_power" : result.exported_power.get_average(),
+			"covered_needs"  : (result.total_production / result.total_consumption).get_average()
+		}
+		results.append(result)
+	if (i == 0):
+			print("thread 0 has finished", j+1, "out of", len(params_to_sim))
+	sim_results.append(results)
+
+processes = []
+for i in range(PARAMS["thread_count"]):
+	print("trying to start thread ", i)
+	process = Process(target=sim_process_function, args=(i, thread_sims_to_do[i], thread_sim_params[i], thread_results))
+	process.start()
+	processes.append(process)
+
+for i in range(len(processes)):
+	processes[i].join()
+	print("process", i, "finished")
+print("all threads have finished")
+print(thread_results[0][0])
+print(thread_results[0][1])
+with open(out_file_path, "w" ) as out_file:
+	print("wind turbines(W/house)", "solar pannels(W/house)", "bioenergy(W/house)", "battery(Wh/house)", "flexibility (raw ratio)", sep=";", file=out_file, end="")
+	print("","import_avg(W/house)", "export_avg(W/house)", "autosufficiency(%)", sep=";", file=out_file)
+	for results in thread_results:
+		for result in results:
+			print(result["wind_to_sim"],
+			    result["sun_to_sim"],
+				result["bio_to_sim"],
+				result["battery_to_sim"],
+				result["flex_to_sim"],
+				result["imported_power"],
+				result["exported_power"],
+				result["covered_needs"] * 100,
+				sep=";",
+				file=out_file)
+
+
