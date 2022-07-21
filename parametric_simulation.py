21c6a12886572c6328b504a4b9d588d8e9375cf6
youen froger <youen.froger2@gmail.com>
2022-07-21 15:30:54 +0200

    edit of parametric sim to launch sim

--- parametric_simulation.py
+++ parametric_simulation.py
@@ -8,24 +8,24 @@ t0 = time()
 PARAMS = {
     "wind_min"               : 1000 * 90  / (365 * 24), #average wind prod in MW
     "wind_max"               : 1000 * 300 / (365 * 24),
-    "wind_nb_points"         : 3,
+    "wind_nb_points"         : 41,
     "sun_min"                : 1000 * 10  / (365 * 24), #average solar prod in MW
     "sun_max"                : 1000 * 300 / (365 * 24),
-    "sun_nb_points"          : 3,
+    "sun_nb_points"          : 61,
     "bio_min"                : 1000 * 6  / (365 * 24), #average bioenergy prod in MW (Methanol)
     "bio_max"                : 1000 * 60 / (365 * 24),
-    "bio_nb_points"          : 5,
+    "bio_nb_points"          : 11,
     "flex_min"               : 0.0, #flexibility in %
-    "flex_max"               : 0.1,
-    "flex_nb_points"         : 3,
+    "flex_max"               : 0.15,
+    "flex_nb_points"         : 16,
     "battery_min"            : 0, #battery capacity in MWh
-    "battery_max"            : 5,
-    "battery_nb_points"      : 3,
+    "battery_max"            : 100,
+    "battery_nb_points"      : 11,
     "scaling_factor_for_pop" : 1e6 / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION),
 	"RES_cons"               : (config.CA_PONTCHATEAU_RES_CONSUMPTION + config.CA_REDON_RES_CONSUMPTION) / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION),
 	"PRO_cons"               : (config.CA_PONTCHATEAU_PRO_CONSUMPTION + config.CA_REDON_PRO_CONSUMPTION) / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION),
 	"ENT_cons"               : (config.CA_PONTCHATEAU_ENT_CONSUMPTION + config.CA_REDON_ENT_CONSUMPTION) / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION),
-    "thread_count"           : 8,
+    "thread_count"           : 15,
 	"begin"                  : datetime.strptime("01/01/2021 00:00","%d/%m/%Y %H:%M"),
 	"end"                    : datetime.strptime("01/01/2022 00:00","%d/%m/%Y %H:%M"),
 	"scale_before_slice"     : False
